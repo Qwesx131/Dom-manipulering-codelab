a5858cf3b31929de3cd3797bf91dd277d80945c4
Qwesx131 <132337567+Qwesx131@users.noreply.github.com>
2023-05-16 12:54:35 +0200

    Add files via upload

---
+++ Dom-manipulering-codelab-main/assets/js/opgaver/domelementer.js
@@ -0,0 +1,122 @@
+/* DOMcelementer */
+
+
+
+/* Opgave 1.1
+Du skal finde elementet med id="redFigure" og console logge det.
+Du skal finde elementet med id="yellowFigure" og console logge det. 
+*/
+
+// din kode her
+
+let redFigur = document.getElementById("redFigure");
+let yellowFigur = document.getElementById("yellowFigure");
+
+console.log(redFigur);
+console.log(yellowFigur);
+
+
+/* opgave 1.2
+Du skal ændre baggrundsfarve på de to elementer du har fundet i opgave 1.1
+farven på elementerne skal være blå. brug evt. elelement.style.backgroundColor
+*/
+
+
+// din kode her
+redFigur.style.backgroundColor = "blue";
+yellowFigur.style.backgroundColor = "blue";
+
+
+/* opgave 2.1
+Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
+- et h2 med teksten: Opgave 2.1 løsning
+- et p med teksten: Jeg har løst opgave 2.1
+*/
+
+
+// din kode her
+let opgaveTwo = document.getElementById("opgaveTwo");
+
+let h2Element = document.createElement("h2");
+h2Element.textContent = "Opgave 2.1 Løsning"
+
+let pElement = document.createElement("p");
+pElement.textContent = "Jeg har løst opgave 2.1";
+
+
+opgaveTwo.appendChild(h2Element);
+opgaveTwo.appendChild(pElement);
+
+
+// opgave 3 liveHTML lists
+/* opgave 3.1
+Du skal finde alle elementer med klassen purpleFigures og console logge resultatet
+*/
+
+// din kode her
+let purpleFigures = document.getElementsByClassName("purpleFigures");
+
+console.log(purpleFigures);
+
+/* opgave 3.2
+Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
+til et array hvor du kan bruge map funktionen til at ændre baggrundsfarven til rød.
+array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+*/
+
+// din kode her
+
+let purpleFigure = document.getElementsByClassName("purpleFigures");
+let purpleFiguresArray = Array.from(purpleFigure);
+
+purpleFiguresArray.map((element) => {
+  element.style.backgroundColor = "red";
+});
+
+/* opgave 3.3
+Brug myLiveList til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
+myElement.children[0].innerHTML til at ændre 1. child elements inner html.
+*/
+
+
+// din kode her
+
+purpleFiguresArray.forEach((myElement) => {
+  myElement.children[0].innerHTML = "RED";
+});
+
+
+/* opgave 4
+opret DOM elementer i elementet med id opgaveFour, udfra data objectet myData.
+Du skal vælge element tags der giver mening i forbindelse med HTML5 semantisk tagging.
+*/
+
+
+
+const myData = {
+    name: 'tiger',
+    image: 'assets/img/standard_tiger.jpg',
+    description: 'En tiger er et stort, katteagtig rovdyr med orange og sort striber i pelsen og skarpe tænder og kløer.'
+};
+
+
+// din kode her
+const opgaveFourElement = document.getElementById('opgaveFour');
+
+// Opretter elementer
+const nameElement = document.createElement('h2');
+nameElement.textContent = myData.name;
+
+const imageElement = document.createElement('img');
+imageElement.src = myData.image;
+
+const descriptionElement = document.createElement('p');
+descriptionElement.textContent = myData.description;
+
+// Tilføjer elementer til opgaveFourElement
+opgaveFourElement.appendChild(nameElement);
+opgaveFourElement.appendChild(imageElement);
+opgaveFourElement.appendChild(descriptionElement);
+
+
+
